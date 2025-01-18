2d5b09b8ced5fbcd74212918f52c9d7e706649ae
Kai Farley <joltaik13@gmail.com>
2025-01-18 14:25:56 -0600

    Adds new subsystem classes and some method outlines (#7)
    * subsystem classes
    
    * subsystem-class-update
    
    * subsystem updates v2
    
    * subsystems update v2
    
    * updated subsystems v3
    
    * subsystem class changes v4
    
    * subsystem changes v5

---
+++ src/main/java/frc/robot/subsystems/Manipulator.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Manipulator extends SubsystemBase {
+    private boolean hasCoral;
+
+    /** Creates a new Manipulator. */
+    public Manipulator() {}
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+
+    enum ManipulatorState {
+        LOADED,
+        SHOOTING,
+        WAITING
+    }
+
+    ManipulatorState state = ManipulatorState.WAITING;
+
+    public ManipulatorState getState() {
+        return state;
+    }
+
+    // Shooting changes state of manipulator to shooting, then waiting since no coral is detected by sensor
+    public void shoot() {}
+}
