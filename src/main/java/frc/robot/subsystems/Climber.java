059187e5e7ab84d73370875a7a4229dcd5bfa39a
joltaik <joltaik13@gmail.com>
2025-01-25 13:45:16 -0600

    Climber and Grabber subsystems v2

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,79 +6,74 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climber extends SubsystemBase {
 
-    private SparkMax winchMotor;
-    private SparkMax cagePullerMotorLeft;
-    private SparkMax cagePullerMotorRight;
+    private double targetAngle;
 
-    // private LaserCan lasercan
-    // private RelativeEncoder climbEncoder;
+    private SparkMax winchMotor;
+
+    private DutyCycleEncoder climberEncoder;
+    private static final int ENCODER_CHANNEL = 1;
 
     private int CLIMB_MOTOR;
-    private int LEFT_PULLER_MOTOR;
-    private int RIGHT_PULLER_MOTOR;
-    private double WINCH_MOTOR_SPEED;
-    private double PULLER_MOTOR_SPEED;
+    private double WINCH_MOTOR_SPEED = 1;
+    private double WINCH_MAX_ACCELERATION = 1;
+    private double WINCH_MAX_VELOCITY = 1;
 
-    public boolean cageDetected;
-    private ClimberState climberState;
+    private ProfiledPIDController retractWinchPID;
+    public static final double RETRACT_PID_P_COEFFICIENT = 0;
+    public static final double RETRACT_PID_I_COEFFICIENT = 0;
+    public static final double RETRACT_PID_D_COEFFICIENT = 0;
+    private TrapezoidProfile.Constraints profileConstraints;
+
+    private ProfiledPIDController deployWinchPID;
+    public static final double DEPLOY_PID_P_COEFFICIENT = 0;
+    public static final double DEPLOY_PID_I_COEFFICIENT = 0;
+    public static final double DEPLOY_PID_D_COEFFICIENT = 0;
 
     /** Creates a new Climber. */
     public Climber() {
-        climberState = ClimberState.WAITING;
+
         winchMotor = new SparkMax(CLIMB_MOTOR, MotorType.kBrushless);
-        cagePullerMotorLeft = new SparkMax(LEFT_PULLER_MOTOR, MotorType.kBrushless);
-        cagePullerMotorRight = new SparkMax(RIGHT_PULLER_MOTOR, MotorType.kBrushless);
+
+        climberEncoder = new DutyCycleEncoder(ENCODER_CHANNEL);
+
+        profileConstraints = new TrapezoidProfile.Constraints(WINCH_MAX_VELOCITY, WINCH_MAX_ACCELERATION);
+        deployWinchPID = new ProfiledPIDController(
+                RETRACT_PID_P_COEFFICIENT, RETRACT_PID_I_COEFFICIENT, RETRACT_PID_D_COEFFICIENT, profileConstraints);
+
+        retractWinchPID = new ProfiledPIDController(
+                DEPLOY_PID_P_COEFFICIENT, DEPLOY_PID_I_COEFFICIENT, DEPLOY_PID_D_COEFFICIENT, profileConstraints);
     }
 
     @Override
     public void periodic() {
 
-        // This method will be called once per scheduler run
-        /*
-         * Starts in a Waiting state
-         * Driver drives into cage
-         * Cage goes into Activated state
-         *
-         */
-        if (climberState == ClimberState.WAITING) {}
-
-        if (climberState == ClimberState.GRABBING) {
-            cagePullerMotorLeft.setVoltage(12 * PULLER_MOTOR_SPEED);
-            cagePullerMotorLeft.setVoltage(12 * PULLER_MOTOR_SPEED);
+        double motorPower;
+        // will change comparison when we know how encoder works
+        if (getCurrentAngle() > getTarget()) {
+            motorPower = deployWinchPID.calculate(getCurrentAngle(), getTarget());
+        } else {
+            motorPower = retractWinchPID.calculate(getCurrentAngle(), getTarget());
         }
 
-        if (climberState == ClimberState.WINCHING) {
-            winchMotor.setVoltage(12 * WINCH_MOTOR_SPEED);
-        }
-    }
-
-    enum ClimberState {
-        WAITING,
-        GRABBING,
-        WINCHING,
-        DONE
+        winchMotor.setVoltage(motorPower);
     }
 
     public double getCurrentAngle() {
-        return 1;
+        return climberEncoder.get() * 360;
     }
 
     public double getTarget() {
-        return 1;
+        return targetAngle;
     }
 
-    public void setTarget(double angle) {}
-
-    public boolean isCageDetected() {
-
-        return false;
-    }
-
-    public ClimberState getState() {
-        return climberState;
+    public void setTarget(double angle) {
+        targetAngle = angle;
     }
 }
