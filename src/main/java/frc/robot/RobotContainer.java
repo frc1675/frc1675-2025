ece94c951d6e4ff5e10d0b51d43a42739f3a2eac
Ij <ijatfg1956@gmail.com>
2025-01-25 14:50:03 -0600

    Add hopper methods (#9)
    * Hopper
    
    * HopperFixes
    
    * Hopper
    
    * Hopper Deluxe

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,9 +5,13 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.operation.JasonDriverConfiguration;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.operation.OperationConfiguration;
+import frc.robot.operation.SimulatorConfiguration;
+import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Hopper.HopperState;
 import java.util.ArrayList;
 
 /**
@@ -23,6 +27,7 @@ public class RobotContainer {
 
     private final CommandXboxController driverController;
     private final CommandXboxController operatorController;
+    private final Hopper hopper;
 
     private ArrayList<OperationConfiguration> operationConfigs = new ArrayList<>();
 
@@ -35,10 +40,11 @@ public class RobotContainer {
         operatorController = new CommandXboxController(1);
         initOperationConfigs();
         registerRobotFunctions();
+        hopper = new Hopper();
     }
 
     private void initOperationConfigs() {
-        operationConfigs.add(new JasonDriverConfiguration(driverController));
+        operationConfigs.add(new SimulatorConfiguration(driverController));
     }
 
     private void registerRobotFunctions() {
@@ -62,5 +68,17 @@ public class RobotContainer {
     public Command getAutonomousCommand() {
         // An example command will be run in autonomous
         return null;
+    }
+
+    public void registerTurnHopperOn(Trigger t) {
+        t.onTrue(new InstantCommand(() -> hopper.changeState(HopperState.ON)));
+    }
+
+    public void registerTurnHopperOff(Trigger t) {
+        t.onTrue(new InstantCommand(() -> hopper.changeState(HopperState.OFF)));
+    }
+
+    public void registerTurnHopperReverse(Trigger t) {
+        t.onTrue(new InstantCommand(() -> hopper.changeState(HopperState.REVERSE)));
     }
 }
--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -4,29 +4,53 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Hopper extends SubsystemBase {
 
+    private SparkMax hopperMotor;
+    private HopperState hopperCurrentState;
+    // intake speed, and speed when intake would go reverse
+    private static final double HOPPER_INTAKE_SPEED = 1.0;
+    private static final double HOPPER_REVERSE_SPEED = -1.0;
+
     /** Creates a new Hopper. */
-    public Hopper() {}
+    public Hopper() {
+        hopperMotor = new SparkMax(0, MotorType.kBrushless); // Replace CAN ID with constant
+        hopperCurrentState = HopperState.OFF;
+        ShuffleboardTab tab = Shuffleboard.getTab("Hopper Display");
+        tab.add("Hopper Shuffleboard", 3);
+    }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        if (getState() == HopperState.OFF) {
+            hopperMotor.setVoltage(0);
+        }
+        if (getState() == HopperState.ON) {
+            hopperMotor.setVoltage(HOPPER_INTAKE_SPEED * 12.0);
+        }
+        if (getState() == HopperState.REVERSE) {
+            hopperMotor.setVoltage(HOPPER_REVERSE_SPEED * 12.0);
+        }
     }
 
-    enum HopperState {
+    public enum HopperState {
         ON,
         OFF,
         REVERSE
     }
 
-    HopperState state = HopperState.OFF;
-
-    public void changeState(Hopper.HopperState hopperState) {}
+    public void changeState(Hopper.HopperState hopperState) {
+        hopperCurrentState = hopperState;
+    }
 
     public HopperState getState() {
-        return state;
+        return hopperCurrentState;
     }
 }
