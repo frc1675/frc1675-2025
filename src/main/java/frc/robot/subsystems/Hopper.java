5ba344a4bf1d01909eed21d9fac85513449735af
IjwasHere <ijatfg1956@gmail.com>
2025-01-25 14:09:41 -0600

    Hopper Deluxe

--- src/main/java/frc/robot/subsystems/Hopper.java
+++ src/main/java/frc/robot/subsystems/Hopper.java
@@ -6,6 +6,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Hopper extends SubsystemBase {
@@ -13,13 +15,15 @@ public class Hopper extends SubsystemBase {
     private SparkMax hopperMotor;
     private HopperState hopperCurrentState;
     // intake speed, and speed when intake would go reverse
-    private static final double HOPPER_INTAKE_SPEED = 12.0;
-    private static final double HOPPER_REVERSE_SPEED = -12.0;
+    private static final double HOPPER_INTAKE_SPEED = 1.0;
+    private static final double HOPPER_REVERSE_SPEED = -1.0;
 
     /** Creates a new Hopper. */
     public Hopper() {
         hopperMotor = new SparkMax(0, MotorType.kBrushless); // Replace CAN ID with constant
         hopperCurrentState = HopperState.OFF;
+        ShuffleboardTab tab = Shuffleboard.getTab("Hopper Display");
+        tab.add("Hopper Shuffleboard", 3);
     }
 
     @Override
@@ -29,14 +33,14 @@ public class Hopper extends SubsystemBase {
             hopperMotor.setVoltage(0);
         }
         if (getState() == HopperState.ON) {
-            hopperMotor.setVoltage(HOPPER_INTAKE_SPEED);
+            hopperMotor.setVoltage(HOPPER_INTAKE_SPEED * 12.0);
         }
         if (getState() == HopperState.REVERSE) {
-            hopperMotor.setVoltage(HOPPER_REVERSE_SPEED);
+            hopperMotor.setVoltage(HOPPER_REVERSE_SPEED * 12.0);
         }
     }
 
-    enum HopperState {
+    public enum HopperState {
         ON,
         OFF,
         REVERSE
